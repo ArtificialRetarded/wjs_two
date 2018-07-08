8fd16b8348fd9f17dd709f82a07b5e6e65772efb
Wayne <1014334379@qq.com>
2018-07-08 19:26:36 +0800

    第一次提交，完成顶部，头部导航只是做了模板，做了轮播图，小屏时导航会把轮播图挤下去的bug

---
+++ js/wjs_index.js
@@ -0,0 +1,44 @@
+$(function () {
+    getData();
+    tabShow();
+    isScroll();
+})
+
+function getData() {
+    var isMobile = true;
+    $.ajax({
+        type: "get",
+        url: "./data/carousel.json",
+        success: function (res) {
+            if ($(window).width() > 768) {
+                isMobile = false;
+            }
+            var strhtml = template("carTem", {
+                "isMobile": isMobile,
+                "list": res
+            });
+            $(".carousel-inner").html(strhtml);
+            // 轮播图小圆点
+            var generic = '';
+            for (var i = 0; i < res.length; i++) {
+                generic += '<li data-target="#carousel-example-generic" data-slide-to="' + i + '" class="' + (i == 0 ?"active":"" ) + '"></li>';
+            }
+            $('.carousel-indicators').html(generic);
+        }
+    })
+}
+
+function tabShow(){
+    $(".wjs_product .row .nav-tabs li a").click(function(e){
+        e.preventDefault();
+        $(this).tab('show');
+    })
+}
+
+function isScroll(){
+    var myScroll = new IScroll('.wrapper',{
+        scrollX:true,
+        scrollY:false,
+        click:true
+    });
+}
